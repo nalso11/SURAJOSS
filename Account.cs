0d89f62c521fa64d31dac9ee48ce2010aca4a22c
surajdhande <91136528+surajdhande@users.noreply.github.com>
2022-07-08 15:42:20 +0530

    Add File Account Class

---
+++ Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+
+    //Delegates are used to define events
+    //Step 1
+    public delegate void EventHandler();
+    internal class Account
+    {
+        public double Balance { get; set; }
+
+        //Step 2
+        public EventHandler UnderBalance;
+        public EventHandler OverBalance;
+     
+        public void Withdraw(double amount)
+        {
+            double result = Balance - amount;
+            if (result < 5000)
+            {
+                //raise ( or trigger) event UnderBalance
+                UnderBalance();  //we are calling event
+            }
+            else
+            {
+                Balance = result;
+            }
+        }
+        public void Deposit(double amount)
+        {
+            double result = Balance +amount;
+            if (result >= 350000)
+            {
+                //raise event OverBalance
+                OverBalance();
+            }
+            else
+            {
+                Balance = result;
+            }
+        }
+    }
+}
